2557bad902a83b7a8961aa722538baa75e5c0512
liupei <1161307442@qq.com>
2022-11-05 22:00:09 +0800

    add login & regist blueprint into app.py

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask
 from views import test
 import config
 from modules.post_info import post_info, db
+from modules.login_regist.login import login_blue
+from modules.login_regist.regist import regist_blue
 
 app = Flask(__name__)
 # register blueprint
 app.register_blueprint(test.test)
 app.register_blueprint(post_info)
+app.register_blueprint(login_blue)
+app.register_blueprint(regist_blue)
 # load config file
 app.config.from_object(config)
 db.init_app(app)
