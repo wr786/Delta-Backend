a21be28b08f27ecd85f661d0bc3bf6cc59212694
liupei <1161307442@qq.com>
2022-11-11 23:46:21 +0800

    set app.secret_key; add blueprint getsesinfo_blue and logout_blue

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask,session
 from views import test
 import config
 from modules.post_info import post_info, db
 from modules.login_regist.login import login_blue
 from modules.login_regist.regist import regist_blue
+from modules.login_regist.getsesinfo import getsesinfo_blue
+from modules.login_regist.logout import logout_blue
 
 app = Flask(__name__)
+app.secret_key=config.SECRET_KEY
 # register blueprint
 app.register_blueprint(test.test)
 app.register_blueprint(post_info)
 app.register_blueprint(login_blue)
 app.register_blueprint(regist_blue)
+app.register_blueprint(getsesinfo_blue)
+app.register_blueprint(logout_blue)
 # load config file
 app.config.from_object(config)
 db.init_app(app)
