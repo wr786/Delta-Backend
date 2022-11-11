a1386e423d7a9563ad178d13f474193be256940f
liupei <1161307442@qq.com>
2022-11-11 19:01:34 +0800

    use common config

--- modules/login_regist/connect.py
+++ modules/login_regist/connect.py
@@ -1,6 +1,6 @@
 #数据库连接配置
 import pymysql
-from ...config_demo import HOST,PORT,PASSWORD,DATABASE,USERNAME
+from ...config import HOST,PORT,PASSWORD,DATABASE,USERNAME
 
 conn = pymysql.connect(
         host=HOST,
