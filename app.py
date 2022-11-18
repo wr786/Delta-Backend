1f433477239c0b6faed18c09fdeb8f83a4de2b03
royalneverwin <55973858+royalneverwin@users.noreply.github.com>
2022-11-18 12:27:06 +0800

    fix a bug in open_post

--- app.py
+++ app.py
@@ -59,16 +59,16 @@ def change_page(message):
 
 @socketio.on('Open Post Info', namespace='/detail')
 def open_post(message):
-    res = search_post_info(id=message['id'])
+    res, _ = search_post_info(id=message['id'])
     if res == []:
         print('No such id!')
-        emit('post_info_response', {'result':'Open Post Failure', 'id':None, 'headline':None, 'tags':None, 'info':None, 'picture':None})
+        emit('post_info_response', {'result':'Open Post Failure', 'id':None, 'headline':None, 'tags':None, 'price_and_number': None, 'info':None, 'picture':None})
     elif len(res) > 1:
         print('Same id for post info!')
-        emit('post_info_response', {'result':'Open Post Failure', 'id':None, 'headline':None, 'tags':None, 'info':None, 'picture':None})
+        emit('post_info_response', {'result':'Open Post Failure', 'id':None, 'headline':None, 'tags':None, 'price_and_number': None, 'info':None, 'picture':None})
     else:
         res = res[0] # 肯定只有一个
-        emit('post_info_response', {'result':'Open Post Success', 'id': res.id, 'headline': res.headline, 'tags': res.tags, 'info': res.info, 'picture': res.picture})
+        emit('post_info_response', {'result':'Open Post Success', 'id': res.id, 'headline': res.headline, 'tags': res.tags, 'price_and_number': res.price_and_number, 'info': res.info, 'picture': res.picture})
 
 if __name__ == '__main__':
     # with app.app_context():
